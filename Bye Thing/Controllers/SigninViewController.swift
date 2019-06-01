7d6a5b4989b1051cad1ab4606e1f21586f58a03a
Aaron Lin <aaronlin851117@gmail.com>
2019-06-01 14:48:42 +0800

    Add sign-in feature and handle sign-in error

--- Bye Thing/Controllers/SigninViewController.swift
+++ Bye Thing/Controllers/SigninViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SigninViewController: UIViewController {
 
@@ -25,7 +26,7 @@ class SigninViewController: UIViewController {
     }
     
     @IBAction func signinButtonPressed(_ sender: Any) {
-        
+        signinWithEmailAndPassword()
     }
     
     @IBAction func dismissButtonPressed(_ sender: Any) {
@@ -33,6 +34,59 @@ class SigninViewController: UIViewController {
         emailTextField.endEditing(true)
     }
     
+    func signinWithEmailAndPassword() {
+        guard let email = emailTextField.text, emailTextField.text != "" else {
+            let alert = UIAlertController(title: "Email must not be empty.", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            emailTextField.becomeFirstResponder()
+            return
+        }
+        guard let password = passwordTextField.text, passwordTextField.text != "" else {
+            let alert = UIAlertController(title: "Password must not be empty.", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "ok", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            passwordTextField.becomeFirstResponder()
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+            if let error = error {
+                switch (error as NSError).code {
+                case AuthErrorCode.wrongPassword.rawValue:
+                    let alert = UIAlertController(title: "Wrong Email or Password", message: "Please refill email or password", preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+                    alert.addAction(alertAction)
+                    self.present(alert, animated: true, completion: nil)
+                case AuthErrorCode.invalidEmail.rawValue:
+                    let alert = UIAlertController(title: "Invalid Email", message: "Email address is malformed", preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "ok", style: .default, handler: { (action) in
+                        alert.dismiss(animated: true, completion: nil)
+                        self.emailTextField.becomeFirstResponder()
+                    })
+                    alert.addAction(alertAction)
+                    self.present(alert, animated: true, completion: nil)
+                default:
+                    let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+                    alert.addAction(alertAction)
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
     
+}
 
+extension SigninViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            textField.resignFirstResponder()
+            self.passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            textField.endEditing(true)
+            signinWithEmailAndPassword()
+        }
+        return true
+    }
 }
